6d198d7d710d726b4d40a060f089e8b576d8e7d5
Andy Pan <andypan1989@hotmail.com>
2021-09-25 19:31:20 +0800

    implement static HTML

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-import DatePicker from "./datePicker/index.js";
+import DatePicker from "./datePicker";
 
 ReactDom.render(<DatePicker />, document.getElementById("app"));
--- src/datePicker/index.js
+++ src/datePicker/index.js
@@ -5,9 +5,52 @@ import "./index.scss";
 class DatePicker extends React.Component {
   render() {
     return (
-      <>
-        <h1>Hello Word!</h1>
-      </>
+      <section id="date-picker">
+        <div class="header">
+          <div class="btn">&lt;</div>
+          <div class="info">Sep 2021</div>
+          <div class="btn">&gt;</div>
+        </div>
+        <div class="week">
+          <span>Su</span>
+          <span>Mo</span>
+          <span>Tu</span>
+          <span>We</span>
+          <span>Th</span>
+          <span>Fr</span>
+          <span>Sa</span>
+        </div>
+        <ul class="col-7">
+          <li class="disable">1</li>
+          <li>2</li>
+          <li>3</li>
+          <li class="today">4</li>
+          <li class="select">5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>10</li>
+          <li>11</li>
+          <li>12</li>
+          <li>13</li>
+          <li>14</li>
+        </ul>
+        <ul class="col-4">
+          <li class="disable">1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li class="select">5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>10</li>
+          <li>11</li>
+          <li>12</li>
+        </ul>
+      </section>
     );
   }
 }
