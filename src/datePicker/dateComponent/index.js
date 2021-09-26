3b9a4d8b0499b0e2bc5575027ae319b7a1ca3aff
Andy Pan <andypan1989@hotmail.com>
2021-09-26 13:02:21 +0800

    implement date's component props

--- src/datePicker/dateComponent/index.js
+++ src/datePicker/dateComponent/index.js
@@ -1,23 +1,55 @@
 "use strict";
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./date.module.scss";
 import WeekComponent from "./weekComponent";
 
 const DATE_LENGTH = 7 * 6;
 
 class DateComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    let y = props.year;
+    let m = props.month;
+
+    let selectObj = new Date(props.select),
+      selectYear = selectObj.getFullYear(),
+      selectDate = selectObj.getDate(),
+      isSelectThisYear = selectYear == y,
+      isSelectThisMonth = false;
+
+    console.log(props.select);
+
+    if (isSelectThisYear) {
+      let selectMonth = selectObj.getMonth();
+
+      isSelectThisMonth = selectMonth == m;
+    }
+
+    this.state = {
+      year: props.year,
+      month: props.month,
+      selectDate: selectDate,
+      isSelectThisMonth: isSelectThisMonth,
+    };
+  }
+
   render() {
-    const preDateList = (y, m) => {
-      let firstDateOfMonth = new Date(y, m, 1),
+    const { year, month, selectDate, isSelectThisMonth } = this.state;
+
+    const preDateList = () => {
+      let firstDateOfMonth = new Date(year, month, 1),
+        preMonth = month - 1,
         diff = firstDateOfMonth.getDay(); // return day of the week
 
       let result = [];
       while (diff > 0) {
-        let preDate = new Date(y, m - 1, 1 - diff);
+        let preDate = new Date(year, preMonth, 1 - diff);
         let date = preDate.getDate();
 
         result.push({
-          id: `${m - 1}_${date}`,
+          id: `${preMonth}_${date}`,
           className: styles.disable,
           text: date,
         });
@@ -28,30 +60,32 @@ class DateComponent extends React.Component {
       return result;
     };
 
-    const thisDateList = (y, m) => {
+    const thisDateList = () => {
       let today = new Date(),
         todayOfYear = today.getFullYear(),
         todayOfMonth = today.getMonth(),
         todayOfDate = today.getDate();
 
-      let lastOfDate = new Date(y, m + 1, 0).getDate();
+      let lastOfDate = new Date(year, month + 1, 0).getDate();
 
-      let checkToday = false;
-      if (todayOfYear === y && todayOfMonth === m) {
-        checkToday = true;
+      let checkTodayOfDate = false;
+      if (todayOfYear == year && todayOfMonth == month) {
+        checkTodayOfDate = true;
       }
 
       let result = [];
       let dateInfo = 1;
       while (dateInfo <= lastOfDate) {
         let classNameArr = [];
-        if (checkToday && todayOfDate === dateInfo) {
+        if (checkTodayOfDate && todayOfDate == dateInfo) {
           classNameArr.push(styles.today);
+        }
+        if (isSelectThisMonth && selectDate == dateInfo) {
           classNameArr.push(styles.select);
         }
 
         result.push({
-          id: `${m}_${dateInfo}`,
+          id: `${month}_${dateInfo}`,
           key: dateInfo,
           className: classNameArr.join(" "),
           text: dateInfo,
@@ -64,18 +98,14 @@ class DateComponent extends React.Component {
     };
 
     const dataList = () => {
-      let currentDate = new Date(),
-        y = currentDate.getFullYear(),
-        m = currentDate.getMonth();
-
-      let result = [...preDateList(y, m), ...thisDateList(y, m)];
+      let result = [...preDateList(), ...thisDateList()];
 
       let lessLength = DATE_LENGTH - result.length;
 
       let nextDate = 1;
       while (lessLength > 0) {
         result.push({
-          id: `${m + 1}_${nextDate}`,
+          id: `${month + 1}_${nextDate}`,
           className: styles.disable,
           text: nextDate,
         });
@@ -101,5 +131,16 @@ class DateComponent extends React.Component {
     );
   }
 }
+
+DateComponent.propTypes = {
+  year: PropTypes.string,
+  month: PropTypes.string,
+  select: PropTypes.string,
+};
+DateComponent.defaultProps = {
+  year: "2021",
+  month: "8",
+  select: "2021-9-27",
+};
 
 export default DateComponent;
