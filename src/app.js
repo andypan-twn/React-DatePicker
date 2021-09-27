a5635b46842d782b648d0fa5b961a30aa00f593a
Andy Pan <andypan1989@hotmail.com>
2021-09-27 21:41:33 +0800

    implement display and close date picker

--- src/app.js
+++ src/app.js
@@ -4,21 +4,39 @@ import DatePicker from "./datePicker";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    let defaultDate = new Date().toISOString().slice(0, 10);
+
     this.state = {
-      defaultDate: defaultDate,
-      date: defaultDate,
+      displayDatePicker: false,
+      date: new Date().toISOString().slice(0, 10),
     };
   }
 
   render() {
+    const { displayDatePicker } = this.state;
+
     const processInputChange = (e) => {
       let changeValue = e.target.value;
-      this.setState({
-        defaultDate: changeValue,
-        date: changeValue,
+      this.setState((state) => {
+        state.date = changeValue;
+        return state;
       });
     };
+
+    const handleFocus = () => {
+      this.setState((state) => {
+        state.displayDatePicker = true;
+        return state;
+      });
+    };
+
+    const handleSelect = (dateStr) => {
+      this.setState((state) => {
+        state.displayDatePicker = false;
+        state.date = dateStr;
+        return state;
+      });
+    };
+
     return (
       <>
         <input
@@ -26,13 +44,11 @@ class App extends React.Component {
           name="date"
           value={this.state.date}
           onChange={processInputChange}
+          onFocus={handleFocus}
         />
-        <DatePicker
-          selectDate={this.state.defaultDate}
-          onSelect={(dateStr) => {
-            this.setState({ date: dateStr });
-          }}
-        />
+        {displayDatePicker && (
+          <DatePicker selectDate={this.state.date} onSelect={handleSelect} />
+        )}
       </>
     );
   }
